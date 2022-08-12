25d06332e4dfb5860ffcc2cd2aa76f6d001c5251
aakash013srivastava <aakash013srivastava@gmail.com>
2022-08-12 16:57:38 +0530

    Book insertion code added

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
-from flask import Flask,flash,render_template,redirect,session
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,flash,render_template,redirect,session,request
 import os
+from flask_mysqldb import MySQL,MySQLdb
+# from flask_sqlalchemy import SQLAlchemy
 
 app =Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/bookrentals.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/bookrentals.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy(app)
 
 
-BOOK_FOLDER = os.path.join('static', 'images')
-app.config['UPLOAD_FOLDER'] = BOOK_FOLDER
+# BOOK_FOLDER = os.path.join('static', 'images')
+# app.config['UPLOAD_FOLDER'] = BOOK_FOLDER
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True, nullable=False)
-    isbn = db.Column(db.Integer, unique=True, nullable=False)
-    author = db.Column(db.String(80),nullable=False)
-    year = db.Column(db.Integer,nullable=False)
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(80), unique=True, nullable=False)
+#     isbn = db.Column(db.Integer, unique=True, nullable=False)
+#     author = db.Column(db.String(80),nullable=False)
+#     year = db.Column(db.Integer,nullable=False)
 
-    def __repr__(self):
-        return '<Book %s --by:%s>'.format(self.title,self.author)
+#     def __repr__(self):
+#         return '<Book %s --by:%s>'.format(self.title,self.author)
 
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'BookRentals'
+
+mysql = MySQL(app)
+
+app.secret_key= 'mysecretkey'
 
 @app.route('/')
 def index():
@@ -41,9 +51,29 @@ def register():
 def login():
     return render_template('login.html')
 
-@app.route('/rent_book')
+@app.route('/rent_book',methods=['GET','POST'])
 def rent_book():
-    return render_template('rent_book.html')
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        try:
+            cursor.execute(
+                'SELECT * FROM Books WHERE isbn = % s', (request.form['isbn']))
+            account = cursor.fetchone()
+        except:
+            print("Error")
+        else:
+            if account == '0' or account == 0:
+                try:
+                    cursor.execute(
+                        'INSERT into FROM Books(title,isbn,author,year) values(%s,%s,%s,%s)',
+                        (request.form['title'],request.form['isbn'],request.form['author'],request.form['year']))
+                except:
+                    print("Error while creating book record")
+                    flash("Could not add book to database")
+        finally:
+            cursor.close()
+        return render_template('rent_book.html')
+    return render_template('rent_book.html')    
 
 @app.route('/admin')
 def admin():
