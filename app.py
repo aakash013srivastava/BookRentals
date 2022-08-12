1c196a7bbfe79d3fa96f70671eaf0fa934e397ec
aakash013srivastava <aakash013srivastava@gmail.com>
2022-08-12 07:50:55 +0530

    Default index route added

--- app.py
+++ app.py
@@ -3,7 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 app =Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/bookrentals.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80),nullable=False)
 
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
