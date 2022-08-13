4fb626e3f15e7893a188e76f88f4b2a62858bc75
aakash013srivastava <aakash013srivastava@gmail.com>
2022-08-13 20:48:03 +0530

    Upload file functionality added

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from shelve import DbfilenameShelf
 from flask import Flask,flash,render_template,redirect,session,request,flash
+from werkzeug.utils import secure_filename
+
 import os
 
 
-
 app =Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+UPLOAD_FOLDER = 'static/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -27,17 +32,26 @@ def login():
 
 @app.route('/rent_book/<title>',methods=['GET','POST'])
 def rent_book(title):
-    if request.method == 'POST':
-        
-        
-        return render_template('rent_book.html')
-    else:
-        print(title)
 
-        return redirect('/')    
 
-@app.route('/admin')
+    print(title)
+    f = open('request.txt','a')
+    f.write("email:"+"aakash@gmail.com"+",title:"+title+"\n")
+    f.close()
+    return redirect('/')    
+
+@app.route('/admin',methods=['GET','POST'])
 def admin():
+    if request.method == 'POST':
+        img = request.files['formFile']
+        if img.filename != '':
+             
+            img.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename)))
+        f = open('books.txt','a')
+        f.write("title:"+request.form['title']+",isbn:"+request.form['isbn']+",author:"+request.form['author']+",year:"+request.form['year']+",fileName:"+request.files['formFile'].filename+"\n")
+        f.close()
+        flash('Book was added to DB')
+        return redirect('/')
     return render_template('admin.html')
 
 
