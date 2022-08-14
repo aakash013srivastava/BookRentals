3a045f5debe9ceb1c3020370edda18c5e0835998
aakash013srivastava <aakash013srivastava@gmail.com>
2022-08-14 08:03:06 +0530

    Register functionality completed

--- app.py
+++ app.py
@@ -7,9 +7,24 @@ import os
 
 app =Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
+app.config['SECRET_KEY'] = '12345'
 UPLOAD_FOLDER = 'static/images/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def check_registration():
+    f = open('Users.txt','r')
+    flag = False
+    lines = f.readlines()
+    for line in lines:
+        email = (line.split(',')[0]).split(':')[1]
+        password = (line.split(',')[1]).split(':')[1]
+        if request.form['exampleInputEmail1'] == email:
+            return True
+    f.close()
+    return False
+        
+
 
 @app.route('/')
 def index():
@@ -25,25 +40,33 @@ def about():
 @app.route('/register',methods=['GET','POST'])
 def register():
     if request.method == "POST":
+        isRegistered = check_registration()
+        if not isRegistered:
+            f = open('Users.txt','a')
+            f.write("email:"+request.form['exampleInputEmail1']+",password:"+request.form['exampleInputPassword1']+"\n")
+            f.close()
+            
+        else:
+            flash('User Already registered')
+        return redirect('/login')
+    return render_template('register.html')
+    
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
         f1 = open('Users.txt','a')
         if os.stat('Users.txt').st_size != 0:
             ls = f1.readlines()
             if len(ls):
                 for line in ls:
                     l1=line.split(",")
-                    l2=l1.split(':')
-                    print(l2)
+                    for l in l1:
+                        l2=l1.split(':')
+                        print(l2)
+                    
         f1.close()
-        
-        f = open('Users.txt','a')
-        f.write("email:"+request.form['exampleInputEmail1']+",password:"+request.form['exampleInputPassword1']+"\n")
-        f.close()
-        return redirect('/login')
-    return render_template('register.html')
-    
-
-@app.route('/login')
-def login():
+        return redirect('/')
     return render_template('login.html')
 
 @app.route('/rent_book/<title>',methods=['GET','POST'])
